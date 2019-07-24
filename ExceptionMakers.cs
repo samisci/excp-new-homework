99fa43bc5c2dae855c5f92e54a47e5f5040357d5
samisci <47791400+samisci@users.noreply.github.com>
2019-07-25 00:08:52 +0300

    Add files via upload

---
+++ ExceptionMakers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class ExceptionMakers
+    {
+        public int GetNumber()
+        {
+            if (int.TryParse(Console.ReadLine(), out int N)) return N;
+            throw new HackerUFormatException("BAD FORMAT");
+        }
+
+        public int[] GetNumbers()
+        {
+            int[] numbers = new int[4];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("<" + i + ">");
+                if (i == numbers.Length)
+                {
+                    Console.ReadLine();
+                    throw new HackerUIndexOutOfRangeException("RANGE VIOLATION");
+                }
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            return numbers;
+        }
+
+        public void DivideByZero()
+        {
+            int x = 0;
+            if (x == 0) throw new HackerUDivideByZeroException("Divided by zero err");
+            x = 10 / x;
+        }
+    }
+}
---
+++ dHackerUDivByZero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class HackerUFormatException : Exception
+    {
+        public HackerUFormatException(string messege) : base(messege)
+        {
+
+        }
+    }
+    class HackerUIndexOutOfRangeException : Exception
+    {
+        public HackerUIndexOutOfRangeException(string messege) : base(messege)
+        {
+
+        }
+    }
+    class HackerUDivideByZeroException : Exception
+    {
+        public HackerUDivideByZeroException(string messege) : base(messege)
+        {
+
+        }
+    }
+}
